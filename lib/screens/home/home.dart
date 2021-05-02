72a4445a2778b0e88adcf7165b14685e3d3c327d
Yuval Kasner <Yuvaldubi1@gmail.com>
2021-05-02 16:59:39 +0300

    generating video to database file

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:isl_translator/services/database.dart';
+import 'package:isl_translator/shared/constant.dart';
+import 'package:isl_translator/shared/loading.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+  bool loading = false;
+
+  String key = '';
+  String url = '';
+  String desc = '';
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +28,55 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.lightBlue[100],
       ),
       backgroundColor: Colors.brown[200],
-      body: Text("Hello. this is the home", style: TextStyle(fontSize: 30.0),),
+      body: Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget> [
+          SizedBox(height: 20.0,),
+          TextFormField(
+              decoration: textInputDecoration.copyWith(hintText: 'title'),
+              validator: (val) => val.isEmpty ? '' : null,
+            onChanged: (val) { // key
+                setState(() {
+                  key = val;
+                });
+               }),
+          SizedBox(height: 20.0,),
+          TextFormField( // password
+            decoration: textInputDecoration.copyWith(hintText: 'url'),
+            onChanged: (val) {
+              setState(() {
+                url = val;
+              });
+            },
+          ),
+          SizedBox(height: 20.0,),
+          TextFormField( // description
+            decoration: textInputDecoration.copyWith(hintText: 'description'),
+            onChanged: (val) {
+              setState(() {
+                desc = val;
+              });
+            },
+          ),
+          SizedBox(height: 20.0,),
+          RaisedButton(
+            color: Colors.pink[400],
+            child: Text("Add video",
+                style: TextStyle(color: Colors.white)
+            ),
+            onPressed: () async {
+              if (_formKey.currentState.validate()){
+                print("$key, $url, $desc");
+                DatabaseService().updateVideo(key, url, desc);
+              }
+            },
+          ),
+          SizedBox(height: 12.0,),
+          Text(error, style: TextStyle(color: Colors.red, fontSize: 14.0),)
+        ],
+      ),
+    ),
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Database {
+class DatabaseService {
+
+  // DatabaseService( { this.uid } );
+
+  // collection reference
+  final CollectionReference videosCollection =
+    Firestore.instance.collection('videosUrls');
+
+  Future updateVideo(String key, String url, String desc) async {
+    return await videosCollection.document('K5PJZ8WUsbNqi7tqfxzmbljgPW73').setData({
+      "title" : key,
+      "url" : url,
+      "description" : desc
+    });
+  }
 
 }
