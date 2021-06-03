6caa66a8fec1b6b4b302a01ae6edb5815114a2b5
Yuval Kasner <Yuvaldubi1@gmail.com>
2021-06-03 17:33:55 +0300

    LruCache bytes read start working on LRUcache.

--- lib/services/video_cache.dart
+++ lib/services/video_cache.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:disk_lru_cache/disk_lru_cache.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 
 class LruCache{
   final LruMap<String,String> map = LruMap();
@@ -33,6 +37,32 @@ class LruCache{
   Future<String> readCache(String key) async{
     CacheSnapshot snapshot = await cache.get(key);
     return await snapshot.getString(0);
+  }
+
+  // Future<void> writeCacheBytes(String key, String value) async {
+  //   CacheEditor editor = await cache.edit(key);
+  //   if (editor == null) {
+  //     return null;
+  //   }
+  //   HttpClient client = new HttpClient();
+  //   HttpClientRequest request = await client.openUrl("GET", Uri.parse(
+  //       "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1534075481&di=1a90bd266d62bc5edfe1ce84ac38330e&src=http://photocdn.sohu.com/20130517/Img376200804.jpg"));
+  //   HttpClientResponse response = await request.close();
+  //   Stream<List<int>> stream = await editor.copyStream(0, response);
+  //   // The bytes has been written to disk at this point.
+  //
+  //   await StreamBuilder(builder: stream).
+  //   // await new ByteStream(stream).toBytes();
+  //   await editor.commit();
+  // }
+
+  Future<Uint8List> readBytes() async{
+    CacheSnapshot snapshot =  await cache.get('imagekey');
+    return await snapshot.getBytes(0);
+  }
+
+  Future<void> clean() async {
+    return await this.cache.clean();
   }
 
 }
