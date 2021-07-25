813c353b731e56eec57f502cdb6e086c1999457d
israelameg <59085159+israelameg@users.noreply.github.com>
2021-07-25 13:31:10 +0300

    add words to the dictionary

--- lib/screens/dictonary/subject.dart
+++ lib/screens/dictonary/subject.dart
@@ -9,6 +9,7 @@ Each subject in the dict has a subject-dict of words that belong to this specifi
 */
 class Subject extends StatefulWidget {
   final String name;
+
   Subject(this.name);
 
   @override
@@ -16,12 +17,18 @@ class Subject extends StatefulWidget {
 }
 
 class _SubjectState extends State<Subject> {
-  VideoPlayerDemo videoPlayerDemo = VideoPlayerDemo(key: Key("0"), myUrls: [],);
+  VideoPlayerDemo videoPlayerDemo = VideoPlayerDemo(
+    key: Key("0"),
+    myUrls: [],
+  );
   List<String> myUrls;
   int ind = 1;
   String subjectName;
   String sentence;
-  VideoPlayer2 _videoFetcher = VideoPlayer2(key: UniqueKey(), sentence: null,);
+  VideoPlayer2 _videoFetcher = VideoPlayer2(
+    key: UniqueKey(),
+    sentence: null,
+  );
 
   @override
   void initState() {
@@ -35,176 +42,334 @@ class _SubjectState extends State<Subject> {
     if (sentence == "") {
       sentence = null;
     }
-    print("sentence is == $sentence");
     if (mounted) {
       setState(() {
-        this._videoFetcher = VideoPlayer2(key: UniqueKey(), sentence: sentence,);
+        this._videoFetcher = VideoPlayer2(
+          key: UniqueKey(),
+          sentence: sentence,
+        );
       });
     }
   }
 
   List<Card> _buildGridCards(BuildContext context) {
     List<ButtonImage> products = [];
-      if(subjectName == "holidays")
-        {
-          ButtonImage holiday = new ButtonImage("","חג","חג");
-          ButtonImage custom = new ButtonImage("","מנהג","מנהג");
-          ButtonImage newYear = new ButtonImage("","ראש השנה","ראש השנה");
-          products.addAll([holiday,custom,newYear]);
-        }
-      else if(subjectName == "pronoun")
-        {
-          ButtonImage she = new ButtonImage("","היא","היא");
-          products.addAll([she]);
-        }
-      else if(subjectName == "animals")
-      {
-        // words
-        ButtonImage dolphin = new ButtonImage("dolphin.jfif","דולפין","דולפין");
-        ButtonImage ant = new ButtonImage("","נמלה","נמלה");
-        ButtonImage hedgehog = new ButtonImage("","קיפוד","קיפוד");
-        ButtonImage snake = new ButtonImage("","נחש","נחש");
-        ButtonImage frog = new ButtonImage("","צפרדע","צפרדע");
-        ButtonImage donkey = new ButtonImage("","חמור","חמור");
-        ButtonImage horse = new ButtonImage("","סוס","סוס");
-        // add words to list
-        products.addAll([dolphin,ant,hedgehog,snake,frog,donkey,horse]);
+    if (subjectName == "holidays") {
+      List names = [
+        "חג",
+        "מנהג",
+        "ראש השנה",
+        "סוכות",
+        "חנוכה",
+        "פורים",
+        "פסח",
+        "יום העצמאות",
+        "לג בעומר"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
       }
-      else if(subjectName == "shapes")
-      {
-        // words
-        ButtonImage circle = new ButtonImage("","עיגול","עיגול");
-        // add words to list
-        products.addAll([circle]);
-      }
-      else if(subjectName == "body")
-      {
-        // words
-        ButtonImage braids = new ButtonImage("","צמות","צמות");
-        // add words to list
-        products.addAll([braids]);
-      }
-      else if(subjectName == "time")
-      {
-        // words
-        ButtonImage minute = new ButtonImage("","דקה","דקה");
-        // add words to list
-        products.addAll([minute]);
-      }
-      else if(subjectName == "geography")
-      {
-        ButtonImage eilat = new ButtonImage("","אילת","אילת");
-        ButtonImage ashdod = new ButtonImage("","אשדוד","אשדוד");
-        ButtonImage givatayim = new ButtonImage("","גבעתיים","גבעתיים");
-        ButtonImage kibbutz = new ButtonImage("","קיבוץ","קיבוץ");
-        products.addAll([eilat,ashdod,givatayim,kibbutz]);
-      }
-      else if(subjectName == "food")
-      {
-        ButtonImage pomegranate = new ButtonImage("","רימון","רימון");
-        products.addAll([pomegranate]);
-      }
+    } else if (subjectName == "pronoun") {
+      List names = [
+        "אותנו",
+        "היא",
+        "הוא",
+        "אותך",
+        "אתה",
+        "את",
+        "אותי",
+        "אני",
+        "לנו",
+        "הם",
+        "הן",
+        "שלכן",
+        "שלכם",
+        "שלנו",
+        "בשבילה",
+        "בשבילו",
+        "שלה",
+        "שלו",
+        "בשבילך",
+        "שלך",
+        "בשבילי",
+        "שלי",
+        "אותן",
+        "אותם",
+        "לך",
+        "לי",
+        "לה",
+        "לכם",
+        "זה",
+        "זאת",
+        "אנחנו"
+      ];
 
-      if (products == null || products.isEmpty) {
-        return const <Card>[];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
       }
+    } else if (subjectName == "animals") {
+      List names = [
+        "דולפין",
+        "נמלה",
+        "קיפוד",
+        "נחש",
+        "צפרדע",
+        "חמור",
+        "סוס",
+        "ארנב",
+        "פרפר",
+        "שפן",
+        "דוב",
+        "אריה",
+        "נמר",
+        "זאב",
+        "תנין",
+        "לווייתן",
+        "ציפור",
+        "דבורה",
+        "זבוב",
+        "יתוש",
+        "ינשוף",
+        "תוכי",
+        "אייל",
+        "צבי",
+        "עכביש",
+        "טווס",
+        "פיל"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
+      }
+    } else if (subjectName == "shapes") {
+      List names = [
+        "עיגול",
+        "ריבוע",
+        "מלבן",
+        "משולש",
+        "טרפז",
+        "מעוין",
+        "משושה",
+        "מעגל",
+        "צורה"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
+      }
+    } else if (subjectName == "body") {
+      List names = [
+        "פנים",
+        "עיניים",
+        "אוזניים",
+        "אף",
+        "יד",
+        "רגל",
+        "סנטר",
+        "עורף",
+        "מרפק",
+        "מצח",
+        "גרון",
+        "שיניים",
+        "דם",
+        "כבד",
+        "כליות",
+        "ריאות",
+        "לב",
+        "עצם"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
+      }
+    } else if (subjectName == "time") {
+      List names = [
+        "דקה",
+        "שנייה",
+        "בוקר",
+        "לפני הצהריים",
+        "צהריים",
+        "אחרי הצהריים",
+        "ערב",
+        "לילה",
+        "יום",
+        "אתמול",
+        "שלשום",
+        "היום",
+        "מחר",
+        "מחרתיים",
+        "שבוע",
+        "שבועיים",
+        "חודש",
+        "שנה",
+        "ינואר",
+        "פבואר",
+        "מארס",
+        "אפריל",
+        "מאי",
+        "יוני",
+        "יולי",
+        "אוגוסט",
+        "ספטמבר",
+        "אוקטובר",
+        "נובמבר",
+        "דצמבר",
+        "יום ראשון",
+        "יום שני",
+        "יום שלישי",
+        "יום רביעי",
+        "יום חמישי",
+        "יום שישי",
+        "יום שבת",
+        "מוצאי שבת",
+        "עכשיו"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
+      }
+    } else if (subjectName == "geography") {
+      List names = [
+        "תל אביב יפו",
+        "רמת גן",
+        "ירושלים",
+        "אילת",
+        "חיפה",
+        "כפר סבא",
+        "פתח תקווה",
+        "ראשון לציון",
+        "חדרה",
+        "רעננה",
+        "אשדוד",
+        "גבעתיים",
+        "דימונה",
+        "זיכרון יעקב",
+        "ראש העין",
+        "מודיעין",
+        "קיבוץ",
+        "מושב"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
+      }
+    } else if (subjectName == "food") {
+      List names = [
+        "כריך",
+        "קציצה",
+        "בורקס",
+        "עוגייה",
+        "פיצה",
+        "מרק",
+        "אורז",
+        "גלידה",
+        "מלח",
+        "פלפל שחור",
+        "בטטה",
+        "תפוח אדמה",
+        "עגבנייה",
+        "מלפפון",
+        "בננה",
+        "תפוח",
+        "אגס",
+        "שזיף",
+        "תאנה",
+        "לימון",
+        "סופגנייה",
+        "רימון",
+        "אוזן המן",
+        "מופלטה"
+      ];
+      for (int i = 0; i < names.length; i++) {
+        products.add(new ButtonImage("", names[i], names[i]));
+      }
+    }
 
-      final ThemeData theme = Theme.of(context);
-      /*final NumberFormat formatter = NumberFormat.simpleCurrency(
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    /*final NumberFormat formatter = NumberFormat.simpleCurrency(
         locale: Localizations.localeOf(context).toString());*/
 
-      return products.map((product) {
-        return Card(
-          //color: Colors.cyan[800],
-          shape: RoundedRectangleBorder(
-              side: BorderSide(color: Colors.cyan[800], width: 2),
-              borderRadius: const BorderRadius.all(
-                  Radius.circular(8.0)
-              )),
-          // TODO: Adjust card heights (103)
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: <Widget>[
-              Column(
-                // TODO: Center items on the card (103)
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          // TODO: Align labels to the bottom and center (103)
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          // TODO: Change innermost Column (103)
-                          children: <Widget>[
-                            // TODO: Handle overflowing labels (103)
-                            Container(alignment: Alignment.center,
-                              child: Text(
-                                product.text,
-                                style: theme.textTheme.title,
-                                maxLines: 2,
-                              ),
+    return products.map((product) {
+      return Card(
+        //color: Colors.cyan[800],
+        shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.cyan[800], width: 2),
+            borderRadius: const BorderRadius.all(Radius.circular(8.0))),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              product.text,
+                              style: theme.textTheme.title,
+                              textDirection: TextDirection.rtl,
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
-              Positioned(
-                left: 0.0,
-                top: 0.0,
-                bottom: 0.0,
-                right: 0.0,
-                child: Material(
-                  type: MaterialType.transparency,
-                  child: InkWell(
-                      onTap: (){
-                        sentence = product.onTap;
-                        playVideos();
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) => _buildPopupDialog(context),
-                        );
-                      }
-                  ),
                 ),
+              ],
+            ),
+            Positioned(
+              left: 0.0,
+              top: 0.0,
+              bottom: 0.0,
+              right: 0.0,
+              child: Material(
+                type: MaterialType.transparency,
+                child: InkWell(onTap: () {
+                  sentence = product.onTap;
+                  playVideos();
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        _buildPopupDialog(context),
+                  );
+                }),
               ),
-            ],
-          ),
-        );
-      }).toList();
-    }
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
 
   Widget _buildPopupDialog(BuildContext context) {
     return Dialog(
       backgroundColor: Colors.white,
-      insetAnimationDuration:
-      const Duration(milliseconds: 100),
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)),
-      child: Container(       // use container to change width and height
+      insetAnimationDuration: const Duration(milliseconds: 100),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: Container(
+        // use container to change width and height
         height: 400,
         width: 1000,
         child: Column(
           children: <Widget>[
             Container(
-            child: AspectRatio(
-                aspectRatio: 1.0,
-                child: _videoFetcher
-               // child: videoPlayerDemo.myUrls.length < 1 ? null : videoPlayerDemo
-            ),
+              child: AspectRatio(aspectRatio: 1.0, child: _videoFetcher
+                  // child: videoPlayerDemo.myUrls.length < 1 ? null : videoPlayerDemo
+                  ),
             ),
             FlatButton(
               onPressed: () {
-                if (mounted){
-
-                }
+                if (mounted) {}
                 Navigator.of(context).pop();
               },
               child: new Text("סגור"),
@@ -234,7 +399,7 @@ class _SubjectState extends State<Subject> {
           padding: EdgeInsets.all(16.0),
           childAspectRatio: 8.0 / 9.0,
           children: _buildGridCards(context) // Changed code
-      ),
+          ),
     );
   }
 }
@@ -246,10 +411,10 @@ Each button image contain:
 - text to display below to image
 - onTap variable that define which page to open when tapping
 */
-class ButtonImage{
+class ButtonImage {
   String name;
   String text;
   String onTap;
 
-  ButtonImage(this.name, this.text,this.onTap);
+  ButtonImage(this.name, this.text, this.onTap);
 }
