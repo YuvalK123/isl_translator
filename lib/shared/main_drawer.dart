49d8d758f6a8c90e545fbd6130c2ca123c397560
Yuval Kasner <Yuvaldubi1@gmail.com>
2021-08-04 18:43:00 +0300

    delete test file

--- lib/shared/main_drawer.dart
+++ lib/shared/main_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:isl_translator/models/drawer_button.dart';
-import 'package:isl_translator/screens/testDir/file_save_test.dart';
 import '../models/profile_image.dart';
 import 'package:isl_translator/models/user.dart';
 import 'package:isl_translator/screens/add_video/add_video.dart';
@@ -125,12 +124,6 @@ class MainDrawer extends StatelessWidget {
                 onTap: () => pushPage(context, TranslationWrapper()),
                 icon: Icon(Icons.translate),
                 isCurrPage: this.currPage == pageButton.TRANSLATION,
-              ),
-              DrawerButton(
-                title: "דף טומטום",
-                onTap: () => pushPage(context, DummyPage()),
-                icon: Icon(Icons.download_done_outlined),
-                isCurrPage: false,
               ),
               FirebaseAuth.instance.currentUser.isAnonymous ? Container() : DrawerButton(
                   title: "הוסף וידיאו",
